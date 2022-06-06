4e07e830449a2794ff1d563a46928becf234e17d
Raymond WP <rwp1byte@gmail.com>
2022-06-06 09:47:31 +0700

    clean unused import

--- registration/src/main/java/com/teknologiumum/pesto/controller/ApiController.java
+++ registration/src/main/java/com/teknologiumum/pesto/controller/ApiController.java
@@ -3,24 +3,16 @@ package com.teknologiumum.pesto.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.teknologiumum.pesto.database.DatabaseConn;
 import com.teknologiumum.pesto.model.User;
 import com.teknologiumum.pesto.service.UserService;
-
-import io.etcd.jetcd.ByteSequence;
 
 @RestController
 @RequestMapping("/api")
