942a0a2633d37d243ced4cfc41917cc337169688
elianiva <dicha.arkana03@gmail.com>
2022-06-21 08:05:20 +0700

    refactor(landing): pretty logger :p

--- landing/app/logger.ts
+++ landing/app/logger.ts
@@ -1,7 +1,11 @@
+function getTime() {
+  return new Date(Date.now()).toLocaleString("en-GB");
+}
+
 export function info(text: string) {
-  console.info(`\x1b[1;34m[INFO]:\x1b[0m ${text}`);
+  console.info(`${getTime()} \x1b[1;34m[INFO] ➤ \x1b[0;34m ${text}\x1b[0m`);
 }
 
 export function error(text: string) {
-  console.error(`\x1b[1;31m[ERROR]:\x1b[0m ${text}`);
+  console.error(`${getTime()} \x1b[1;31m[ERRO] ➤ \x1b[0;31m ${text}\x1b[0m`);
 }
