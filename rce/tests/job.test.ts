3d36a68c5249dfc1b563e2365cb769170043c0e0
Reinaldy Rafli <aldy505@tutanota.com>
2022-06-21 11:07:20 +0700

    chore: rework everything to uvu

--- rce/tests/job.test.ts
+++ rce/tests/job.test.ts
@@ -1,44 +1,36 @@
-import test from "ava";
-import { Job } from "../src/job/job.js";
-import { Runtime } from "../src/runtime/runtime.js";
-// import { SystemUsers, User } from "../src/user/user.js";
+import { test } from "uvu";
+import * as assert from "uvu/assert";
+import { Job } from "../src/job/job";
+import { Runtime } from "../src/runtime/runtime";
+// import { SystemUsers, User } from "../src/user/user";
 
-test("should throw error on invalid job parameters", (t) => {
+test("should throw error on invalid job parameters", () => {
   const runtime = new Runtime("Javascript", "16.14.0", "js", false, [], ["node", "{file}"], ["node", "js"], {});
 
-  t.throws(
+  assert.throws(
     () => {
       new Job({ uid: 1, gid: 1, free: false, username: "code_executor_1" }, runtime, "");
     },
-    {
-      instanceOf: TypeError,
-      message: "Invalid job parameters"
-    }
+    "Invalid job parameters"
   );
 });
 
-test("should give a default value for timeout and memoryLimit", (t) => {
+test("should give a default value for runTimeout and memoryLimit", () => {
   const runtime = new Runtime("Javascript", "16.14.0", "js", false, [], ["node", "{file}"], ["node", "js"], {});
   const job = new Job({ uid: 1, gid: 1, free: false, username: "code_executor_1" }, runtime, "console.log(\"Hello world~\");");
 
-  if (job.timeout === 5_000) {
-    t.pass("Default value for timeout is 5_000");
-  }
+  assert.equal(job.runTimeout, 5_000);
 
-  if (job.memoryLimit === 128 * 1024 * 1024) {
-    t.pass("Default value for memoryLimit is 128MB");
-  }
+  assert.equal(job.memoryLimit, 128 * 1024 * 1024);
 });
 
-test("should not use default value for timeout and memoryLimit", (t) => {
+test("should not use default value for runTimeout and memoryLimit", () => {
   const runtime = new Runtime("Javascript", "16.14.0", "js", false, [], ["node", "{file}"], ["node", "js"], {});
-  const job = new Job({ uid: 1, gid: 1, free: false, username: "code_executor_1" }, runtime, "console.log(\"Hello world~\");", 10_000, 256 * 1024 * 1024);
+  const job = new Job({ uid: 1, gid: 1, free: false, username: "code_executor_1" }, runtime, "console.log(\"Hello world~\");", 10_000, 10_000, 256 * 1024 * 1024);
 
-  if (job.timeout === 10_000) {
-    t.pass("Default value for timeout is 10_000");
-  }
+  assert.equal(job.runTimeout, 10_000);
 
-  if (job.memoryLimit === 256 * 1024 * 1024) {
-    t.pass("Default value for memoryLimit is 256MB");
-  }
+  assert.equal(job.memoryLimit, 256 * 1024 * 1024);
 });
+
+test.run();
--- rce/tests/runtime.test.ts
+++ rce/tests/runtime.test.ts
@@ -1,26 +1,23 @@
-import test from "ava";
-import { Runtime } from "../src/runtime/runtime.js";
+import { test } from "uvu";
+import * as assert from "uvu/assert";
+import { Runtime } from "../src/runtime/runtime";
 
-test("should throw error on invalid runtime parameters", (t) => {
-  t.throws(
+test("should throw error on invalid runtime parameters", () => {
+  assert.throws(
     () => {
       new Runtime("", "", "", true, [], [], [], {});
     },
-    {
-      instanceOf: TypeError,
-      message: "Invalid runtime parameters"
-    }
+    "Invalid runtime parameters"
   );
 });
 
-test("should throw error on invalid buildCommand parameters whilist being a compiled runtime", (t) => {
-  t.throws(
+test("should throw error on invalid buildCommand parameters whilist being a compiled runtime", () => {
+  assert.throws(
     () => {
       new Runtime("Maven", "1.0.0", ".xml", true, [], ["mvn", "war:explode"], ["mvn"], {});
     },
-    {
-      instanceOf: TypeError,
-      message: "Invalid runtime parameters: buildCommand is empty yet compiled is true"
-    }
+    "Invalid runtime parameters: buildCommand is empty yet compiled is true"
   );
 });
+
+test.run();
