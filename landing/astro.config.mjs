bd902c940b2be45c599c736962e431cb9fc58c8e
elianiva <dicha.arkana03@gmail.com>
2022-07-18 16:38:30 +0700

    style(landing): formatting

--- landing/astro.config.mjs
+++ landing/astro.config.mjs
@@ -4,15 +4,15 @@ import solid from "@astrojs/solid-js";
 // https://astro.build/config
 export default defineConfig({
 	integrations: [solid()],
-  vite: {
-    build: {
-      rollupOptions: {
-        output: {
-          entryFileNames: 'entries/entry.[hash].js',
-          chunkFileNames: 'chunks/chunk.[hash].js',
-          assetFileNames: 'assets/asset.[hash][extname]',
-        },
-      },
-    },
-  },
+	vite: {
+		build: {
+			rollupOptions: {
+				output: {
+					entryFileNames: "entries/entry.[hash].js",
+					chunkFileNames: "chunks/chunk.[hash].js",
+					assetFileNames: "assets/asset.[hash][extname]",
+				},
+			},
+		},
+	},
 });
