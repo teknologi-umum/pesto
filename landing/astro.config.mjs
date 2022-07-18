6992398652c438e16485de0215f74a7bce8e659e
elianiva <dicha.arkana03@gmail.com>
2022-07-18 15:46:03 +0700

    fix(landing): move entry output into subdirectory

--- landing/astro.config.mjs
+++ landing/astro.config.mjs
@@ -1,8 +1,18 @@
 import { defineConfig } from "astro/config";
-
 import solid from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [solid()],
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: 'entries/entry.[hash].js',
+          chunkFileNames: 'chunks/chunk.[hash].js',
+          assetFileNames: 'assets/asset.[hash][extname]',
+        },
+      },
+    },
+  },
 });
