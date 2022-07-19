32a362abf595e6fcb60b7ae803ef8119a85ed2a0
Raymond Wangsa Putra <54213470+RayWP@users.noreply.github.com>
2022-07-19 23:05:28 +0700

    [MODIFY] remove unused import

--- registration/src/main/java/com/teknologiumum/pesto/controller/StatusController.java
+++ registration/src/main/java/com/teknologiumum/pesto/controller/StatusController.java
@@ -1,7 +1,6 @@
 package com.teknologiumum.pesto.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
