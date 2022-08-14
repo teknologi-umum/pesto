2f76567bfe7416725086818f10cb7c6c67082718
Reinaldy Rafli <aldy505@proton.me>
2022-08-14 22:12:26 +0700

    hotfix: wrong installation script for clisp

--- rce/packages/clisp/install.sh
+++ rce/packages/clisp/install.sh
@@ -4,5 +4,6 @@ cd ~
 curl -LO http://prdownloads.sourceforge.net/sbcl/sbcl-2.2.7-x86-64-linux-binary.tar.bz2
 tar -xf sbcl-2.2.7-x86-64-linux-binary.tar.bz2
 cd sbcl-2.2.7-x86-64-linux
-./install
+./install.sh
+cd ~
 rm -rf sbcl-2.2.7-x86-64-linux-binary.tar.bz2 sbcl-2.2.7-x86-64-linux
