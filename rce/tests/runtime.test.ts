decf7e99c2b5f4cdf631224465afd7a33d5735f9
Reinaldy Rafli <aldy505@proton.me>
2022-08-14 13:17:56 +0700

    test(rce): put assertion on memory changes

--- rce/tests/runtime.test.ts
+++ rce/tests/runtime.test.ts
@@ -4,7 +4,7 @@ import { Runtime } from "../src/runtime/runtime.js";
 test("should throw error on invalid runtime parameters", (t) => {
   t.throws(
     () => {
-      new Runtime("", "", "", true, [], [], [], {});
+      new Runtime("", "", "", true, [], [], [], {}, false, 0, 0);
     },
     {
       instanceOf: TypeError,
@@ -16,11 +16,27 @@ test("should throw error on invalid runtime parameters", (t) => {
 test("should throw error on invalid buildCommand parameters whilist being a compiled runtime", (t) => {
   t.throws(
     () => {
-      new Runtime("Maven", "1.0.0", ".xml", true, [], ["mvn", "war:explode"], ["mvn"], { "JAVA_HOME": "/usr/lib/jvm/java-8-openjdk-amd64", "PATH": "/usr/local/bin:/usr/bin:/bin" });
+      new Runtime("Maven", "1.0.0", ".xml", true, [], ["mvn", "war:explode"], ["mvn"], { "JAVA_HOME": "/usr/lib/jvm/java-8-openjdk-amd64", "PATH": "/usr/local/bin:/usr/bin:/bin" }, false, 0, 0);
     },
     {
       instanceOf: TypeError,
       message: "Invalid runtime parameters: buildCommand is empty yet compiled is true"
     }
   );
+});
+
+test("should throw error on invalid memoryLimit parameters", (t) => {
+  t.throws(
+    () => {
+      new Runtime("C", "11", "c", true, ["gcc", "./a.out"], ["./a.out"], ["c"], { "PATH": "/usr/local/bin:/usr/bin:/bin" }, true, -1, 0);
+    }
+  );
+});
+
+test("should throw error on invalid processLimit parameters", (t) => {
+  t.throws(
+    () => {
+      new Runtime("C", "11", "c", true, ["gcc", "./a.out"], ["./a.out"], ["c"], { "PATH": "/usr/local/bin:/usr/bin:/bin" }, true, 256, -1);
+    }
+  )
 });
