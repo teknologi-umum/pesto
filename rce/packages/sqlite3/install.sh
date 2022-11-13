c611064999eb57edbb6aa2c9d79b4db19746333e
Reinaldy Rafli <aldy505@proton.me>
2022-11-13 14:53:13 +0700

    fix(sqlite): repair command execution

--- rce/packages/sqlite3/install.sh
+++ rce/packages/sqlite3/install.sh
@@ -9,3 +9,10 @@ make -j $(nproc)
 make install -j $(nproc)
 cd ..
 rm -rf sqlite-autoconf-3390400.tar.gz sqlite-autoconf-3390400
+cat <<EOF > /opt/sqlite3/exec_sqlite3
+#!/usr/bin/env bash
+
+/opt/sqlite3/bin/sqlite3 < $1
+EOF
+
+chmod +x /opt/sqlite3/exec_sqlite3
