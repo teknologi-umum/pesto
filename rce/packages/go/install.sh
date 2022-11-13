432dd4d8f0a93db8a3d6573b9a93bd4e859e9146
Reinaldy Rafli <aldy505@proton.me>
2022-11-13 22:47:04 +0700

    fix(rce): go package installation (#65)

--- rce/packages/go/install.sh
+++ rce/packages/go/install.sh
@@ -3,5 +3,6 @@
 VERSION="1.18.3"
 
 curl -LO https://go.dev/dl/go${VERSION}.linux-amd64.tar.gz
+mkdir -p /opt/go/1.18
 tar -C /opt/go/1.18 -xzf go${VERSION}.linux-amd64.tar.gz
 rm go${VERSION}.linux-amd64.tar.gz
