5912821e2a77067ef31c8bfedb125abd0703febc
Reinaldy Rafli <aldy505@proton.me>
2022-11-14 20:17:50 +0700

    fix(rce): create directory for each package

--- rce/packages/clisp/install.sh
+++ rce/packages/clisp/install.sh
@@ -4,6 +4,7 @@ cd ~
 curl -LO http://prdownloads.sourceforge.net/sbcl/sbcl-2.2.7-x86-64-linux-binary.tar.bz2
 tar -xf sbcl-2.2.7-x86-64-linux-binary.tar.bz2
 cd sbcl-2.2.7-x86-64-linux
+mkdir -p /opt/sbcl/
 INSTALL_ROOT=/opt/sbcl/2.2.7 ./install.sh
 cd ~
 rm -rf sbcl-2.2.7-x86-64-linux-binary.tar.bz2 sbcl-2.2.7-x86-64-linux
--- rce/packages/sqlite3/install.sh
+++ rce/packages/sqlite3/install.sh
@@ -4,6 +4,7 @@ apt-get install -y unzip
 curl -LO https://www.sqlite.org/2022/sqlite-autoconf-3390400.tar.gz
 tar -zxf sqlite-autoconf-3390400.tar.gz
 cd sqlite-autoconf-3390400
+mkdir -p /opt/sqlite3
 ./configure --prefix=/opt/sqlite3
 make -j $(nproc)
 make install -j $(nproc)
