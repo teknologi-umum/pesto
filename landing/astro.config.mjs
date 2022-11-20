944354b5e7b726a4502a10147d163f885bbdbedc
Reinaldy Rafli <aldy505@proton.me>
2022-11-20 19:24:00 +0700

    chore: run format

--- landing/astro.config.mjs
+++ landing/astro.config.mjs
@@ -4,18 +4,18 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  integrations: [solid()],
-  vite: {
-    build: {
-      rollupOptions: {
-        output: {
-          entryFileNames: "entries/entry.[hash].js",
-          chunkFileNames: "chunks/chunk.[hash].js",
-          assetFileNames: "assets/asset.[hash][extname]"
-        }
-      }
-    }
-  },
-  adapter: node({ mode: "standalone" })
+	output: "server",
+	integrations: [solid()],
+	vite: {
+		build: {
+			rollupOptions: {
+				output: {
+					entryFileNames: "entries/entry.[hash].js",
+					chunkFileNames: "chunks/chunk.[hash].js",
+					assetFileNames: "assets/asset.[hash][extname]",
+				},
+			},
+		},
+	},
+	adapter: node({ mode: "standalone" }),
 });
