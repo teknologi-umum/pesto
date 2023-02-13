5e822571b1440fb75b5292ca19ef136bfeb8787d
Reinaldy Rafli <aldy505@proton.me>
2023-02-13 09:21:56 +0700

    fix(rce): modify java download url to static url

--- rce/packages/java/install.sh
+++ rce/packages/java/install.sh
@@ -2,8 +2,8 @@
 
 cd ~
 
-curl -LO https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.tar.gz
-tar -zxf jdk-17_linux-x64_bin.tar.gz
+curl -LO https://download.oracle.com/java/17/archive/jdk-17.0.6_linux-x64_bin.tar.gz
+tar -zxf jdk-17.0.6_linux-x64_bin.tar.gz
 mkdir -p /opt/java/
-mv -v jdk-17.0.5/ /opt/java/17/
-rm -rf jdk-17_linux-x64_bin.tar.gz
+mv -v jdk-17.0.6/ /opt/java/17/
+rm -rf jdk-17.0.6_linux-x64_bin.tar.gz
