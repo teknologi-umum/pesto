3e661d2b0bfb3c947e6b7502a6399c0a3e243c55
Reinaldy Rafli <aldy505@proton.me>
2023-03-17 22:17:39 +0700

    fix(rce): create janet directory

--- rce/packages/janet/install.sh
+++ rce/packages/janet/install.sh
@@ -4,6 +4,6 @@ VERSION="1.27.0"
 
 curl -LO https://github.com/janet-lang/janet/releases/download/v${VERSION}/janet-v${VERSION}-linux-x64.tar.gz
 tar -zxf janet-v${VERSION}-linux-x64.tar.gz
-
+mkdir -p /opt/janet/
 mv -v janet-v${VERSION}-linux/ /opt/janet/v${VERSION}/
 rm janet-v${VERSION}-linux-x64.tar.gz
