542a78428c26be0b5229d5be79563f3d8bfdabda
Reinaldy Rafli <aldy505@proton.me>
2023-03-17 21:24:59 +0700

    feat(rce): janet support

---
+++ rce/packages/janet/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+VERSION="1.27.0"
+
+curl -LO https://github.com/janet-lang/janet/releases/download/v${VERSION}/janet-v${VERSION}-linux-x64.tar.gz
+tar -zxf janet-v${VERSION}-linux-x64.tar.gz
+
+mv -v janet-v${VERSION}-linux/ /opt/janet/v${VERSION}/
+rm janet-v${VERSION}-linux-x64.tar.gz
