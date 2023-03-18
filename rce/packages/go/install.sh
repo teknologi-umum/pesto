f11fb2ef7c3fbe0f015c2edc30ba784178b5b287
Reinaldy Rafli <aldy505@proton.me>
2023-03-18 15:27:33 +0700

    chore(rce): bump go version (#88)

--- rce/packages/go/install.sh
+++ rce/packages/go/install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-VERSION="1.18.3"
+VERSION="1.20.2"
 
 curl -LO https://go.dev/dl/go${VERSION}.linux-amd64.tar.gz
-mkdir -p /opt/go/1.18
-tar -C /opt/go/1.18 -xzf go${VERSION}.linux-amd64.tar.gz
+mkdir -p /opt/go/1.20
+tar -C /opt/go/1.20 -xzf go${VERSION}.linux-amd64.tar.gz
 rm go${VERSION}.linux-amd64.tar.gz
