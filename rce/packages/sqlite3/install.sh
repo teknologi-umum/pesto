f8d4627c33855d9d21b533f9336dd37a2b04f3bf
Reinaldy Rafli <aldy505@proton.me>
2023-06-03 19:05:06 +0700

    fix(rce): allow sqlite3 to access /bin and /usr/bin

--- rce/packages/sqlite3/install.sh
+++ rce/packages/sqlite3/install.sh
@@ -1,19 +1,28 @@
 #!/usr/bin/env bash
 
 cd ~
+
 curl -LO https://www.sqlite.org/2022/sqlite-autoconf-3390400.tar.gz
 tar -zxf sqlite-autoconf-3390400.tar.gz
+
 cd sqlite-autoconf-3390400
+
 mkdir -p /opt/sqlite3
 ./configure --prefix=/opt/sqlite3
+
 LIBDIR=/opt/sqlite3/lib
+
 export LD_RUN_PATH=$LD_RUN_PATH:$LIBDIR
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$LIBDIR
+
 make -j $(nproc)
 ldconfig
 make install -j $(nproc)
+
 cd ..
+
 rm -rf sqlite-autoconf-3390400.tar.gz sqlite-autoconf-3390400
+
 cat <<EOF > /opt/sqlite3/exec_sqlite3
 #!/usr/bin/bash
 
