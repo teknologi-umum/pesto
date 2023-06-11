8a030c1469ac43288afea3444734a3916754d06b
Reinaldy Rafli <aldy505@proton.me>
2023-06-11 21:19:47 +0700

    fix(rce): directly execute bash

--- rce/packages/sqlite3/install.sh
+++ rce/packages/sqlite3/install.sh
@@ -15,14 +15,18 @@ LIBDIR=/opt/sqlite3/lib
 export LD_RUN_PATH=$LD_RUN_PATH:$LIBDIR
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$LIBDIR
 
+echo "Executing make"
 make -j $(nproc)
+echo "Executing ldconfig"
 ldconfig
+echo "Executing make install"
 make install -j $(nproc)
 
-cd ..
-
+cd ~
+echo "Removing downlaoded files"
 rm -rf sqlite-autoconf-3390400.tar.gz sqlite-autoconf-3390400
 
+echo "Registering exec_sqlite3"
 cat <<EOF > /opt/sqlite3/exec_sqlite3
 #!/usr/bin/bash
 
@@ -30,3 +34,6 @@ cat <<EOF > /opt/sqlite3/exec_sqlite3
 EOF
 
 chmod +x /opt/sqlite3/exec_sqlite3
+
+echo "Let's take a look at what's inside /opt/sqlite3"
+ls /opt/sqlite3
