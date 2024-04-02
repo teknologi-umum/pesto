9dc278972f1c42192ada3c586455f72312cbe1e7
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2024-04-02 23:08:07 +0700

    refactor(auth_rust): remove dynamic dispatch trait and use generic trait

--- auth_rust/src/main.rs
+++ auth_rust/src/main.rs
@@ -1,37 +1,47 @@
+use axum::extract::State;
+use axum::http::{header, HeaderMap, StatusCode};
+use axum::response::IntoResponse;
+use axum::routing::any;
+use axum::{routing::get, Router};
+use redis::aio::MultiplexedConnection;
+use redis::{AsyncCommands, Client};
 use std::error::Error;
 use std::fmt::{Display, Formatter};
-use std::rc::Rc;
-use axum::{Router, routing::{any, get}};
-use axum::http::{header, HeaderMap, StatusCode};
-use axum::response::{IntoResponse};
-use redis::{AsyncCommands, Client, Commands};
+use std::sync::Arc;
 use tokio::net::TcpListener;
 
 async fn healthcheck() -> impl IntoResponse {
     (StatusCode::OK, "OK")
 }
 
-async fn authenticate<R>(headers: HeaderMap) -> impl IntoResponse {
+async fn authenticate<Command: AsyncCommands>(
+    headers: HeaderMap,
+    State(auth_repo): State<AuthRepo<Command>>, // auth_repo: AuthRepo<Command>,
+) -> impl IntoResponse {
     if let Some(token) = headers.get("X-Pesto-Token") {
         todo!();
-        return (StatusCode::OK, [(header::CONTENT_TYPE, "application/json")], "{\"message\":\"OK\"}");
+        return (
+            StatusCode::OK,
+            [(header::CONTENT_TYPE, "application/json")],
+            r#"{"message":"OK\}"#,
+        );
     } else {
         return (
             StatusCode::UNAUTHORIZED,
             [(header::CONTENT_TYPE, "application/json")],
-           "{\"message\":\"Token must be supplied\"}",
+            r#"{"message":"Token must be supplied"}"#,
         );
     }
 }
 
 enum AuthError {
-    TokenNotRegistered
+    TokenNotRegistered,
 }
 
 impl Display for AuthError {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
-            AuthError::TokenNotRegistered => write!(f, "Token not registered")
+            AuthError::TokenNotRegistered => write!(f, "Token not registered"),
         }
     }
 }
@@ -42,36 +52,14 @@ struct TokenValue {
     revoked: bool,
 }
 
-trait AuthRepo {
-    fn acquire_token_value(&self, token: String) -> Result<TokenValue, AuthError>;
-    fn acquire_counter_limit(&self, token_value: TokenValue) -> Result<i64, AuthError>;
-    fn increment_monthly_counter(&self, token_value: TokenValue, counter_limit: i64) -> Result<(), AuthError>;
-}
-
-// Before you say anything, yes I don't have time to think of the correct naming
-// We'll just do it Java-like.
 #[derive(Clone)]
-struct AuthRepoImpl {
-    pub redis_client: Rc<Box<dyn AsyncCommands>>,
+struct AuthRepo<Command: AsyncCommands> {
+    pub redis_client: Arc<Command>,
 }
 
-impl AuthRepoImpl {
-    fn new(redis_client: Rc<Box<dyn AsyncCommands>>) -> Self {
-        return AuthRepoImpl{ redis_client }
-    }
-}
-
-impl AuthRepo for AuthRepoImpl {
-    fn acquire_token_value(&self, token: String) -> Result<TokenValue, AuthError> {
-        todo!()
-    }
-
-    fn acquire_counter_limit(&self, token_value: TokenValue) -> Result<i64, AuthError> {
-        todo!()
-    }
-
-    fn increment_monthly_counter(&self, token_value: TokenValue, counter_limit: i64) -> Result<(), AuthError> {
-        todo!()
+impl<Command: AsyncCommands> AuthRepo<Command> {
+    fn new(redis_client: Arc<Command>) -> Self {
+        return AuthRepo { redis_client };
     }
 }
 
@@ -79,12 +67,13 @@ impl AuthRepo for AuthRepoImpl {
 async fn main() -> Result<(), Box<dyn Error>> {
     let redis_client = Client::open("redis://@localhost:6739")?;
     let redis_async_connection = redis_client.get_multiplexed_async_connection().await?;
-    let rc_client = Rc::new(Box::new(redis_async_connection) as Box<dyn AsyncCommands + Send + Sync>);
+    let rc_client = Arc::new(redis_async_connection);
+    let auth_repo = AuthRepo::new(rc_client);
+
     let app = Router::new()
         .route("/healthz", get(healthcheck))
-        .route("/", any(authenticate))
-        .with_state(rc_client);
-
+        .route("/", any(authenticate::<MultiplexedConnection>))
+        .with_state(auth_repo);
 
     let listener = TcpListener::bind("0.0.0.0:3000").await?;
     axum::serve(listener, app).await?;
