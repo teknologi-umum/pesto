1ed14551f000a48c860fba0279f0e1ef48d610de
Reinaldy Rafli <aldy505@proton.me>
2024-09-09 06:36:53 +0700

    chore: maintenance of september 2024 (#183)

--- rce/scripts/register-users.cjs
+++ rce/scripts/register-users.cjs
@@ -12,65 +12,64 @@ const console = require("console");
 
 // This file should be in CommonJS as it will be called directly.
 
-
 function execute(command, workingDirectory = process.cwd()) {
-    return new Promise((resolve, reject) => {
-        const cmd = cp.exec(
-            command,
-            {cwd: workingDirectory},
-            (error) => {
-                if (error) {
-                    reject(error);
-                }
-            }
-        );
+	return new Promise((resolve, reject) => {
+		const cmd = cp.exec(command, { cwd: workingDirectory }, (error) => {
+			if (error) {
+				reject(error);
+			}
+		});
 
-        let stdout = "";
-        let stderr = "";
+		let stdout = "";
+		let stderr = "";
 
-        cmd.stdout.on("data", (data) => {
-            console.log(data.toString());
-            stdout += data.toString();
-        });
+		cmd.stdout.on("data", (data) => {
+			console.log(data.toString());
+			stdout += data.toString();
+		});
 
-        cmd.stderr.on("data", (data) => {
-            console.error(data.toString());
-            stderr += data.toString();
-        });
+		cmd.stderr.on("data", (data) => {
+			console.error(data.toString());
+			stderr += data.toString();
+		});
 
-        cmd.on("error", (error) => {
-            reject(error);
-        });
+		cmd.on("error", (error) => {
+			reject(error);
+		});
 
-        cmd.on("close", (code) => {
-            if (code !== 0) {
-                reject(new Error(stderr));
-                return;
-            }
+		cmd.on("close", (code) => {
+			if (code !== 0) {
+				reject(new Error(stderr));
+				return;
+			}
 
-            resolve(stdout);
-        });
-    });
+			resolve(stdout);
+		});
+	});
 }
 
 const GLOBAL_GROUP_ID = 64101;
 
 (async () => {
-    await fs.mkdir("/code", {recursive: true});
+	await fs.mkdir("/code", { recursive: true });
 
-    // Create a new group
-    const groupAddStdout = await execute(`groupadd -g ${GLOBAL_GROUP_ID.toString()} code_executors`);
-    console.log(groupAddStdout.toString());
+	// Create a new group
+	const groupAddStdout = await execute(
+		`groupadd -g ${GLOBAL_GROUP_ID.toString()} code_executors`,
+	);
+	console.log(groupAddStdout.toString());
 
-    const ints = Array.from(Array(50).keys());
+	const ints = Array.from(Array(50).keys());
 
-    for await (const i of ints) {
-        const uid = (GLOBAL_GROUP_ID + i).toString();
-        const homeDir = `/code/code_executor_${uid}`;
-        await execute(`useradd -M --base-dir ${homeDir} --uid ${uid} --gid ${GLOBAL_GROUP_ID.toString()} --shell /bin/bash --home ${homeDir} --comment "Code executor ${uid}" code_executor_${uid}`);
-        await fs.mkdir(homeDir, {recursive: true});
-        await execute(`chown -R code_executor_${uid}:code_executors ${homeDir}`);
-        await execute(`chmod 711 ${homeDir}`);
-        console.log(`Successfully register user code_executor_${uid}`);
-    }
+	for await (const i of ints) {
+		const uid = (GLOBAL_GROUP_ID + i).toString();
+		const homeDir = `/code/code_executor_${uid}`;
+		await execute(
+			`useradd -M --base-dir ${homeDir} --uid ${uid} --gid ${GLOBAL_GROUP_ID.toString()} --shell /bin/bash --home ${homeDir} --comment "Code executor ${uid}" code_executor_${uid}`,
+		);
+		await fs.mkdir(homeDir, { recursive: true });
+		await execute(`chown -R code_executor_${uid}:code_executors ${homeDir}`);
+		await execute(`chmod 711 ${homeDir}`);
+		console.log(`Successfully register user code_executor_${uid}`);
+	}
 })();
--- rce/src/job/files.ts
+++ rce/src/job/files.ts
@@ -1,21 +1,28 @@
 type File = {
-    fileName: string;
-    code: string;
-    entrypoint: boolean;
-}
+	fileName: string;
+	code: string;
+	entrypoint: boolean;
+};
 
 export class Files {
-  public readonly files: File[];
-  constructor(files: Array<{ fileName: string, code: string, entrypoint: boolean }>, extension: string) {
-    this.files = [];
+	public readonly files: File[];
+	constructor(
+		files: Array<{ fileName: string; code: string; entrypoint: boolean }>,
+		extension: string,
+	) {
+		this.files = [];
 
-    for (let i = 0; i < files.length; i++) {
-      const file = files[i];
-      if (file.fileName === "") {
-        this.files.push({ fileName: `code${i}.${extension}`, code: file.code, entrypoint: true });
-      } else {
-        this.files.push(file);
-      }
-    }
-  }
+		for (let i = 0; i < files.length; i++) {
+			const file = files[i];
+			if (file.fileName === "") {
+				this.files.push({
+					fileName: `code${i}.${extension}`,
+					code: file.code,
+					entrypoint: true,
+				});
+			} else {
+				this.files.push(file);
+			}
+		}
+	}
 }
