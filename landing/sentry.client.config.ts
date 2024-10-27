067d701b1fc52ab361fd3669a96d732169b688e1
Reinaldy Rafli <aldy505@proton.me>
2024-10-27 21:33:09 +0700

    chore: maintenance october 2024 (#191)

--- landing/sentry.client.config.ts
+++ landing/sentry.client.config.ts
@@ -3,13 +3,15 @@ import * as Sentry from "@sentry/astro";
 Sentry.init({
 	dsn: process.env.SENTRY_DSN ?? "",
 	integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
-	attachStacktrace: true,
-	sampleRate: 1.0,
-	tracesSampleRate: 0.2,
-	tracePropagationTargets: ["localhost", "https://pesto.teknologiumum.com"],
+	sampleRate: Number.parseFloat(process.env.SENTRY_SAMPLE_RATE ?? "1.0"),
+	tracesSampleRate: Number.parseFloat(process.env.SENTRY_TRACES_SAMPLE_RATE ?? "0.5"),
+	tracePropagationTargets: [
+		"localhost",
+		process.env.SENTRY_TRACE_PROPAGATION_TARGET ?? "https://pesto.teknologiumum.com",
+	],
 	// Capture Replay for 10% of all sessions,
 	// plus for 100% of sessions with an error
-	replaysSessionSampleRate: 0.02,
-	replaysOnErrorSampleRate: 0.5,
+	replaysSessionSampleRate: Number.parseFloat(process.env.SENTRY_REPLAYS_SESSION_SAMPLE_RATE ?? "0.02"),
+	replaysOnErrorSampleRate: Number.parseFloat(process.env.SENTRY_REPLAYS_ON_ERROR_SAMPLE_RATE ?? "0.5"),
 	debug: import.meta.env.DEV,
 });
--- landing/sentry.server.config.ts
+++ landing/sentry.server.config.ts
@@ -3,8 +3,7 @@ import * as Sentry from "@sentry/astro";
 Sentry.init({
 	dsn: process.env.SENTRY_DSN ?? "",
 	integrations: [new Sentry.Integrations.Http({ tracing: true })],
-	attachStacktrace: true,
-	sampleRate: 1.0,
-	tracesSampleRate: 0.2,
+	sampleRate: Number.parseFloat(process.env.SENTRY_SAMPLE_RATE ?? "1.0"),
+	tracesSampleRate: Number.parseFloat(process.env.SENTRY_TRACES_SAMPLE_RATE ?? "0.5"),
 	debug: import.meta.env.DEV,
 });
