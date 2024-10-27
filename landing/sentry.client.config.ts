9febe359729dcdfb2f322ab7dc25c6420b4d27ba
Reinaldy Rafli <aldy505@proton.me>
2024-10-27 13:03:31 +0700

    chore: formatting

--- landing/sentry.client.config.ts
+++ landing/sentry.client.config.ts
@@ -5,7 +5,10 @@ Sentry.init({
 	integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
 	sampleRate: Number.parseFloat(process.env.SENTRY_SAMPLE_RATE ?? "1.0"),
 	tracesSampleRate: Number.parseFloat(process.env.SENTRY_TRACES_SAMPLE_RATE ?? "0.5"),
-	tracePropagationTargets: ["localhost", process.env.SENTRY_TRACE_PROPAGATION_TARGET ?? "https://pesto.teknologiumum.com"],
+	tracePropagationTargets: [
+		"localhost",
+		process.env.SENTRY_TRACE_PROPAGATION_TARGET ?? "https://pesto.teknologiumum.com",
+	],
 	// Capture Replay for 10% of all sessions,
 	// plus for 100% of sessions with an error
 	replaysSessionSampleRate: Number.parseFloat(process.env.SENTRY_REPLAYS_SESSION_SAMPLE_RATE ?? "0.02"),
